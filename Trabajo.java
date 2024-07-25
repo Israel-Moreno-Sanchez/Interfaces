390ccc97940c555b7d4ef521c3559e23cdbe7ccc
Israel-Moreno-Sanchez <morenojoseisrael30@gmail.com>
2024-07-25 09:02:44 -0600

    Trabajo.java

---
+++ Trabajo.java
@@ -0,0 +1,12 @@
+package mx.com.cuh;
+
+public abstract class Trabajo {
+	public abstract void checarEntrada();
+	public abstract void trabajar();
+	public abstract void horaDeComida();
+	public abstract void cobrar();
+	public abstract void  checarSalida();
+	
+
+
+}
